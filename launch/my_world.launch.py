75948e91bbc0335d80d5da90aa93d137297addb8
John Brann <john.brann4315@gmail.com>
2024-09-24 16:52:48 -0400

    created launch file for gazebo, fixed issue wth gzserver and gzclient, using gazebo command now instead

---
+++ launch/my_world.launch.py
@@ -0,0 +1,15 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+
+def generate_launch_description():
+    # Path to the Gazebo world file
+    world_path = os.path.join(get_package_share_directory('gazebo_worlds'), 'worlds', 'my_world.world')
+
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', world_path],
+            output='screen'
+        ),
+    ])
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+import glob
 
 package_name = 'gazebo_worlds'
 
@@ -10,16 +11,17 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob.glob('launch/*.launch.py')),
+        ('share/' + package_name + '/worlds', glob.glob('worlds/*.world')), 
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='csrobot',
     maintainer_email='john.brann4315@gmail.com',
-    description='TODO: Package description',
+    description='Package for custom Gazebo worlds',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
