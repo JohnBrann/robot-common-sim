ccc2824c706968a732479d17bd297b3792aec0e9
John Brann <john.brann4315@gmail.com>
2024-09-27 14:33:17 -0400

    changed name of world to launch for current testing

--- launch/my_world.launch.py
+++ launch/my_world.launch.py
@@ -5,7 +5,7 @@ from launch.actions import ExecuteProcess
 
 def generate_launch_description():
     # Path to the Gazebo world file
-    world_path = os.path.join(get_package_share_directory('gazebo_worlds'), 'worlds', 'my_world.world')
+    world_path = os.path.join(get_package_share_directory('gazebo_worlds'), 'worlds', 'base_world.world')
 
     return LaunchDescription([
         ExecuteProcess(
