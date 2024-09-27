93942ca4cbac6ccdc78d5d2864a742a24a1c59e2
John Brann <john.brann4315@gmail.com>
2024-09-27 15:49:44 -0400

    added commmand line argument capability when selecting a world

--- launch/my_world.launch.py
+++ launch/my_world.launch.py
@@ -1,15 +1,29 @@
-import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    # Path to the Gazebo world file
-    world_path = os.path.join(get_package_share_directory('gazebo_worlds'), 'worlds', 'base_world.world')
+    # Declare a launch argument for the world file
+    world_arg = DeclareLaunchArgument(
+        'world_name',
+        default_value='empty.world',
+        description='Specify the world file to load in Gazebo'
+    )
+
+    # Directory for world files
+    worlds_dir = get_package_share_directory('gazebo_worlds') + '/worlds/'
+
+    # Execute Gazebo with the dynamically specified world file
+    gazebo_command = ExecuteProcess(
+        cmd=['gazebo', '--verbose', LaunchConfiguration('world_name')],
+        cwd=[worlds_dir],
+        output='screen'
+    )
 
     return LaunchDescription([
-        ExecuteProcess(
-            cmd=['gazebo', '--verbose', world_path],
-            output='screen'
-        ),
+        # Declare the argument for the world
+        world_arg,
+        # Launch Gazebo with the specified world
+        gazebo_command,
     ])
