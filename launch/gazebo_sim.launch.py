31edbeb9c6feee220fbe3c30f0c28b4b6479fb9b
John Brann <john.brann4315@gmail.com>
2024-11-01 15:50:57 -0400

    fixed pathing to allow for world name command line arguments

--- launch/gazebo_sim.launch.py
+++ launch/gazebo_sim.launch.py
@@ -8,35 +8,32 @@ from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
     # Get paths to packages and files
-    robot_common_sim_dir = get_package_share_directory('robot-common-sim')
+    pkg_robot_common_sim = get_package_share_directory('robot-common-sim')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
 
-    # Declare launch argument for the world name
+    # Declare the launch argument for the world name
     world_arg = DeclareLaunchArgument(
-        'world_name',
-        default_value=os.path.join(robot_common_sim_dir, 'worlds', '4x4m_four_boxes.world'),
-        description='Specify the world file to load in Gazebo'
+        'world', default_value='4x4m_empty.world',
+        description='Name of the Gazebo world to load'
     )
 
-    # World file path from the argument
-    world_path = LaunchConfiguration('world_name')
-
-    # Launch configurations
-    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    # Path to the worlds directory
+    world_dir = os.path.join(pkg_robot_common_sim, 'worlds')
 
     # Gazebo server
     gzserver_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
         ),
-        launch_arguments={'world': world_path, 'use_sim_time': use_sim_time}.items() 
+        launch_arguments={'world': [world_dir + '/', LaunchConfiguration('world')]}.items()
     )
 
     # Gazebo client
     gzclient_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
-        )
+        ),
+        launch_arguments={'world': [world_dir + '/', LaunchConfiguration('world')]}.items()
     )
 
     # Create the launch description and add actions
